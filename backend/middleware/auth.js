38b030c6dc3dc72c78838aa1ebe3c96b40a0965f
Camille Martignac <79940961+Arev-tech@users.noreply.github.com>
2021-12-01 16:56:25 +0100

    auth

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,9 +1,11 @@
 const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
 
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, 'azerfdz2345653534RVFF');
+        const decodedToken = jwt.verify(token, process.env.JWT);
         const userId = decodedToken.userId;
         if(req.body.userId && req.body.userId !== userId) {
             res.status(401).json({ 'error' : 'invalid request' });
